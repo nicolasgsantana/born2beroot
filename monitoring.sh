4ef9ec3b156656f1ddeb0ad929290025d936aabb
nicolasgsantana <nicolas07san@gmail.com>
2025-09-03 15:15:33 -0300

    update to include all terminals

--- monitoring.sh
+++ monitoring.sh
@@ -1,42 +1,48 @@
-#!/bin/bash
+#!/bin/bash	
 
-printf "	#Architecture: "
-uname -a
+arch=$(uname -a)
+physical_cpu=$(lscpu | grep 'Socket(s):' | awk '{print $2}')
+virtual_cpu=$(nproc --all)
+memory_usage=$(free -m | grep Mem: | awk '{ percent= ($7 / $2) * 100; printf "%d/%dMB (%.2f%%)\n", $7, $2, percent}')
+disk_usage=$(df -h --total | grep total | awk '{printf "%s/%s (%s)\n", $3, $2, $5}')
+cpu_load=$(top -b -n 1 | grep 'Cpu(s)'| awk '{ percent = 100 - $8; printf "%.1f%%\n", percent }')
+last_boot=$(who -b | awk '{printf "%s %s\n", $3, $4}')
 
-printf "	#CPU physical : "
-lscpu | grep 'Socket(s):' | awk '{print $2}'
-
-printf "	#vCPU : "
-nproc --all
-
-printf "	#Memory Usage: "
-free -m | grep Mem: | awk '{ percent= ($7 / $2) * 100; printf "%d/%dMB (%.2f%%)\n", $7, $2, percent}'
-
-printf "	#Disk Usage: "
-df -h --total | grep total | awk '{printf "%s/%s (%s)\n", $3, $2, $5}'
-
-printf "	#CPU load: "
-top -b -n 1 | grep 'Cpu(s)'| awk '{ percent = 100 - $8; printf "%.1f%%\n", percent }'
-
-printf "	#Last boot: "
-who -b | awk '{printf "%s %s\n", $3, $4}'
-
-printf "	#LVM use: "
 if [ $(lsblk -no TYPE | grep -c '^lvm$') -gt 0 ]; then
-	printf "yes\n"
+	lvm_use="yes"
 else
-	printf "no\n"
+	lvm_use="no"
 fi
 
 tcp_count=$(ss | grep tcp | wc -l)
-printf "	#Connections TCP : %d ESTABLISHED\n" $tcp_count
-
 user_count=$(w --no-header | wc -l)
-printf "	#User log: %d\n" $user_count
-
 ip_address=$(hostname --ip-address)
 mac_address=$(ip link | grep enp -A 1 | grep link/ether | awk '{print $2}')
-printf "	#Network: IP %s (%s)\n" $ip_address $mac_address
 
-sudo_count=$(sudo cat /var/log/sudo/sudo.log | grep COMMAND | wc -l)
-printf "	#Sudo : %d cmd\n" $sudo_count
+sudo_count=$(echo "obase=10; ibase=36; $(sudo cat /var/log/sudo/seq)" | bc)
+
+msg="\
+	#Architecture: $arch
+	#CPU physical : $physical_cpu
+	#vCPU : $virtual_cpu
+	#Memory Usage: $memory_usage
+	#Disk Usage: $disk_usage
+	#CPU load: $cpu_load
+	#Last boot: $last_boot
+	#LVM use: $lvm_use
+	#Connections TCP : $tcp_count ESTABLISHED
+	#User log: $user_count
+	#Network: IP $ip_address ($mac_address)
+	#Sudo : $sudo_count cmd"
+
+if [ ! -f /tmp/startup-message-displayed ]; then
+
+		echo "$msg" | sudo tee /etc/issue > /dev/null
+		touch /tmp/startup-message-displayed
+fi
+
+ls /dev/pts/* /dev/tty[1-6] | while read -r TTY; do
+	if [ -w "$TTY" ]; then
+		printf "%s\n" "$msg" > "$TTY"
+	fi
+done
